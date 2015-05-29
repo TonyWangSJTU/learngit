8437253c4cff687f6feddc0029edbd14290711cc
Tony <tony@sjtu.edu.cn>
2015-05-29 14:33:14 +0800

    add a cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: Tony Wang
+    > Mail: 0910472@sjtu.edu.cn 
+    > Created Time: Fri May 29 14:31:28 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Great"<<endl;
+}
